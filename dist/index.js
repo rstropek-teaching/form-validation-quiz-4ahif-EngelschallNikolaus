ed274462472385fb34ca51b5bf97968c9c51c8f2
EngelschallNikolaus <31962790+EngelschallNikolaus@users.noreply.github.com>
2017-10-28 14:23:56 +0200

    committing my files

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+"use strict";
+//nothing is filled out yet
+var fn = false;
+var ln = false;
+var em = true;
+//hide all warnings and disable the submit button at the beginning
+$('#firstNameMandatory').hide();
+$('#lastNameMandatory').hide();
+$('#emailMandatory').hide();
+checkSubmit();
+$('#otherMediaChannel').hide();
+//all the events
+//checks the first name field
+$('#firstName').blur(function () {
+    if (!$(this).val()) {
+        $('#firstNameMandatory').show();
+        fn = false;
+    }
+    else {
+        $('#firstNameMandatory').hide();
+        fn = true;
+    }
+    checkSubmit();
+});
+//checks the last name field
+$('#lastName').blur(function () {
+    if (!$(this).val()) {
+        $('#lastNameMandatory').show();
+        ln = false;
+    }
+    else {
+        $('#lastNameMandatory').hide();
+        ln = true;
+    }
+    checkSubmit();
+});
+//checks the email field
+$('#email').blur(function () {
+    checkEmail();
+});
+$('#newsletter').blur(function () {
+    checkEmail();
+});
+function checkEmail() {
+    if (!$('#email').val() && $('input.form-check-input').is(':checked')) {
+        $('#emailMandatory').show();
+        em = false;
+    }
+    else {
+        $('#emailMandatory').hide();
+        em = true;
+    }
+    checkSubmit();
+}
+//check optional selection
+$('#mediaChannelSelect').on("change", function () {
+    if ($('#mediaChannelSelect option:selected').text() == 'Other') {
+        $('#otherMediaChannel').show();
+    }
+    else {
+        $('#otherMediaChannel').hide();
+    }
+});
+//function to check if the submit button should be enabled or disabled
+function checkSubmit() {
+    if (fn && ln && em) {
+        $(':input[type="submit"]').prop("disabled", false);
+    }
+    else {
+        $(':input[type="submit"]').prop("disabled", true);
+    }
+}
---
+++ dist/index.ts
@@ -0,0 +1,69 @@
+//nothing is filled out yet
+let fn: boolean = false;
+let ln: boolean = false;
+let em: boolean = true;
+
+//hide all warnings and disable the submit button at the beginning
+$('#firstNameMandatory').hide();
+$('#lastNameMandatory').hide();
+$('#emailMandatory').hide();
+checkSubmit();
+$('#otherMediaChannel').hide();
+
+//all the events
+//checks the first name field
+$('#firstName').blur(function () {
+    if (!$(this).val()) {
+        $('#firstNameMandatory').show();
+        fn = false;
+    } else {
+        $('#firstNameMandatory').hide();
+        fn = true;
+    }
+    checkSubmit();
+});
+//checks the last name field
+$('#lastName').blur(function () {
+    if (!$(this).val()) {
+        $('#lastNameMandatory').show();
+        ln = false;
+    } else {
+        $('#lastNameMandatory').hide();
+        ln = true;
+    }
+    checkSubmit();
+});
+//checks the email field
+$('#email').blur(function () {
+    checkEmail();
+});
+$('#newsletter').blur(function () {
+    checkEmail();
+});
+function checkEmail() {
+    if (!$('#email').val() && $('input.form-check-input').is(':checked')) {
+        $('#emailMandatory').show();
+        em = false;
+    } else {
+        $('#emailMandatory').hide();
+        em = true;
+    }
+    checkSubmit();
+}
+//check optional selection
+$('#mediaChannelSelect').on("change", function () {
+    if ($('#mediaChannelSelect option:selected').text() == 'Other') {
+        $('#otherMediaChannel').show();
+    } else {
+        $('#otherMediaChannel').hide();
+    }
+});
+//function to check if the submit button should be enabled or disabled
+function checkSubmit() {
+    if (fn && ln && em) {
+        $(':input[type="submit"]').prop("disabled", false);
+    } else {
+        $(':input[type="submit"]').prop("disabled", true);
+    }
+}
+
